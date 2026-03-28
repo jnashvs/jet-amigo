4b3cab953f06fbfa515abf26f51822584ad66102
jnashvs <31808210+jnashvs@users.noreply.github.com>
2026-03-28 15:04:56 +0000

    cause error to test vercel

--- src/App.tsx
+++ src/App.tsx
@@ -51,7 +51,7 @@ export default function App() {
           
           <div className="hidden md:flex gap-8 items-center">
             <a href="#como-funciona" className="text-secondary hover:text-primary transition-colors font-bold tracking-tight font-headline">Como funciona</a>
-            <a href="#pedir" className="text-secondary hover:text-primary transition-colors font-bold tracking-tight font-headline">Pedir viagem</a>
+            <a href="
             <a href="#precos" className="text-secondary hover:text-primary transition-colors font-bold tracking-tight font-headline">Preços</a>
             <button className="bg-primary text-white px-6 py-2.5 rounded-full font-bold hover:opacity-90 transition-all active:scale-95 shadow-lg shadow-primary/20">
               Entrar
